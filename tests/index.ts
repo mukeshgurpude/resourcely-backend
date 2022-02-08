f305d7f2f354763a1d602c2380bc41272665c1da
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-02-08 22:41:54 +0530

    tests: make tests scalable

--- tests/index.ts
+++ tests/index.ts
@@ -5,24 +5,33 @@ import chaiHttp from 'chai-http'
 
 chai.use(chaiHttp)
 
+type Path = {
+  path: string
+  message: string
+  text ?: string
+  json ?: object
+}
+
 suite('Check status of servers', () => {
-  test('Server is running', async () => {
-    return chai
-      .request(app)
-      .get('/status')
-      .then(res => {
-        assert.ok(res)
-        assert.strictEqual(res.status, 200)
-        assert.strictEqual(res.text, 'Ok')
-      })
-  })
-  test('Url shortner is active', async () => {
-    return chai
-      .request(app)
-      .get('/api/shortner')
-      .then(res => {
-        assert.ok(res)
-        assert.strictEqual(res.status, 200)
-      })
+  const paths: Path[] = [{
+    path: '/status',
+    message: 'Server is up and running',
+    text: 'Ok'
+  }, {
+    path: '/api/shortner/',
+    message: 'Url shortner is responding'
+  }]
+
+  paths.forEach(p => {
+    test(p.message, async () => {
+      return chai.request(app)
+        .get(p.path)
+        .then(res => {
+          assert.ok(res)
+          assert.strictEqual(res.status, 200)
+          p.text && assert.strictEqual(res.text, p.text)
+          p.json && assert.deepEqual(res.body, p.json)
+        })
+    })
   })
 })
