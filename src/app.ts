b85794a60320bb4b05530671514cadbf15f28a37
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-02-08 18:55:28 +0530

    security: add helmet in production

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import helmet from 'helmet'
 import { config } from 'dotenv'
 
 config()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'production') app.use(helmet())
 app.use(express.text())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
