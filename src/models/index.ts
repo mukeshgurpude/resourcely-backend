9b93d7744a016426749188a9719e7914b5752b8d
Mukesh Gurpude <55982424+mukeshgurpude@users.noreply.github.com>
2022-02-12 22:15:41 +0530

    Fix testing and database problems (#11)
    * type: multiple fields
    
    * refractor: move connection to server
    
    * tests: fix errors in watch mode
    
    * no need of additional handling

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,22 +1,15 @@
-import { connect } from 'mongoose'
+import { connect, connection } from 'mongoose'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import Url from './url'
 
-export default async function connect_db() {
-  return connect(process.env.MONGO_URL)
-    .catch(err => {
-      console.error('Error connecting to MongoDB... Find more details below')
-      console.log('*'.repeat(50))
-      console.error(err)
-      console.log('*'.repeat(50))
-      process.exit(1)
-    })
-}
 
 export async function mock_db() {
   return MongoMemoryServer.create()
     .then(async (instance) => {
-      await connect(instance.getUri(), {dbName: 'resourcely-test'})
+      // Connect only if not already connected
+      if (connection.readyState === 0) {
+        await connect(instance.getUri(), {dbName: 'resourcely-test'})
+      }
     })
 }
 
