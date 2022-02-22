5a51c14ade3c521e40c915a48c3e48d0b213beef
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-02-22 22:05:11 +0530

    feat: add shortcode before validation

--- src/models/base.ts
+++ src/models/base.ts
@@ -1,8 +1,9 @@
 import { Schema, SchemaDefinition, SchemaOptions } from 'mongoose'
+import generate_id from '@utils/random'
 import Ibase from '@ctypes/base'
 
 
-function schema_factory<Type>(definition: SchemaDefinition, options: SchemaOptions = {}): Schema {
+function schema_factory<Type>(definition: SchemaDefinition, options: SchemaOptions = {}, prefix=''): Schema {
   const schema = new Schema<Ibase | Type>({
     expires_at: {
       type: Date,
@@ -17,6 +18,11 @@ function schema_factory<Type>(definition: SchemaDefinition, options: SchemaOptio
   })
   schema.index({ expires_at: 1 }, { expireAfterSeconds: 0 })
   schema.index({ shortcode: 1 }, { unique: true })
+  schema.pre('validate', function(next) {
+    const doc = this as Ibase
+    doc.shortcode = prefix + generate_id()
+    next()
+  })
   return schema
 }
 
