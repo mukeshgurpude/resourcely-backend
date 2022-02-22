8d197d7f6bde8c1871a2b9b6b8694ca2294b7639
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-02-22 19:25:27 +0530

    feat: add an id generator

---
+++ src/utils/random.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv'
+import { customRandom, random, urlAlphabet } from 'nanoid'
+
+config()
+
+export default function generate_id() {
+  const nanoid = customRandom(urlAlphabet, parseInt(process.env.ID_LENGTH) ?? 8, random)
+  return nanoid()
+}
