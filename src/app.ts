92ba885ac8a9227fcffd250e62750ec0e614ed95
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-03-01 01:49:31 +0530

    🍻 chore: use new logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import helmet from 'helmet'
+import { connectLogger } from 'log4js'
 import apiRouter from '@routes/index'
 import { config } from 'dotenv'
+import logger from '@utils/logger'
 
 config()
 
 const app = express()
 
+app.use(connectLogger(logger, {level: 'auto'}))
 /* istanbul ignore if */
 if (process.env.NODE_ENV === 'production') app.use(helmet())
 app.use(express.text())
