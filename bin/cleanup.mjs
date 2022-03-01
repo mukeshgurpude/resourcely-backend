51da4ac8fb54b1ac258397529595fb331c2bcebc
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-03-01 11:17:24 +0530

    🎨 refractor: separate out delete logic

--- bin/cleanup.mjs
+++ bin/cleanup.mjs
@@ -1,36 +1,13 @@
 #!/usr/bin/env node
 import { config } from 'dotenv'
-import { readdirSync, unlink } from 'fs'
+import { readdirSync } from 'fs'
+import { folder, remove_if_expired } from './utils.mjs'
 config()
 
-let count = 0
-
-/**
- * Remove a file, if it has expired.
- * @param {string} file path to file
- */
-function remove_if_expired(file) {
-  if (name_to_date(file) < Date.now()) {
-    unlink(`${folder}/${file}`, (err) => {
-      if (err) console.error('Error removing file', err)
-      else count += 1
-    })
-  }
-}
-
-/**
- * Decode a filename to its expiry timestamp.
- * @param {string} filename
- * @returns {number} unix timestamp
- */
-function name_to_date(filename) {
-  const [, end] = filename.split('-')
-  const [timestamp,] = end.split('.')
-  return Number(timestamp)
-}
-
-const folder = process.env.UPLOAD_FOLDER || 'uploads'
-const files = readdirSync(folder)
+let files = readdirSync(folder, { withFileTypes: true, encoding: 'utf8' }) // Read folder contents
+  .filter(f => f.isFile())
+  .map(f => f.name)
+console.log(`Found ${files.length} files...`)
 
 files.forEach(remove_if_expired)
-console.log(`Removed ${count} files`)
+// TODO: Count number of removed files
