c2a7a0eb970bb8ae81d904156c5a166f72d12471
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-03-01 18:46:04 +0530

    🎨 use path alias

--- src/models/image.ts
+++ src/models/image.ts
@@ -1,7 +1,7 @@
 import { model, models } from 'mongoose'
+import { SHORTCODE_PREFIXES } from '@utils/constants'
 import IImage from '@ctypes/image'
 import schema_factory from './base'
-import { SHORTCODE_PREFIXES } from '@src/utils/constants'
 
 const image_schema = schema_factory<IImage>({
   image_path: { type: String, required: true }
