ab2d73934c82c137262563f9546cc1d19b3088a2
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-03-01 08:18:17 +0530

    🐛 fix: use unix time

--- src/middlewares/store-file.ts
+++ src/middlewares/store-file.ts
@@ -1,13 +1,21 @@
 import { RequestHandler } from 'express'
 import { rename } from 'fs'
-import separate_extension from '@src/utils/name'
+import separate_extension from '@utils/name'
 
 const store_file: RequestHandler = function(_, res, next) {
   const { filename, expire_time } = res.locals.after_upload
   const { ext, name } = separate_extension(filename)
-  const new_name = `${name}-${expire_time}.${ext}`
 
+  const expire_time_ms = to_unix(expire_time)
+
+  const new_name = `${name}-${expire_time_ms}.${ext}`
   rename(filename, new_name, next)
 }
 
+function to_unix(time: Date|number|string): number {
+  if (time instanceof Date ) return time.getTime()
+  return Number(time)
+}
+
 export default store_file
+export { to_unix }
